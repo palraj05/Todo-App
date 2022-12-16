703ca453ee59e65fc8afd149cc483bdb8ab907ac
unknown <palraz0512@gmail.com>
2022-12-16 11:10:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "../src/Components/App.css"
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import TodosList from "./Components/TodosList";
+import React, {useState, useEffect} from "react";
+
+function App() {
+
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+
+  useEffect (() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  })
+  return (
+    <div className="container">
+        <div className="app-wrapper">
+
+            <div>
+                <Header />
+            </div>
+            <div>
+              <Form
+              input={input}
+              setInput={setInput}
+              todos={todos}
+              setTodos={setTodos}
+              editTodo={editTodo}
+              setEditTodo={setEditTodo}
+               />
+            </div>
+            <div>
+              <TodosList 
+              todos={todos} 
+              setTodos={setTodos}
+              setEditTodo={setEditTodo}
+
+               />
+            </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
